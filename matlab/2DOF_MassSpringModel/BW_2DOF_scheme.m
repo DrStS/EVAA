2f4b3ccbd3c037c85ceaad8cd3ce41ffd21d3ee0
ga78mov <felix-sievers@freenet.de>
2020-01-30 13:58:54 +0100

    generalization bug fix

--- matlab/2DOF_MassSpringModel/BW_2DOF_scheme.m
+++ matlab/2DOF_MassSpringModel/BW_2DOF_scheme.m
@@ -1,5 +1,6 @@
 function [t,y] = BW_2DOF_scheme(t, x_prev, x_curr, aux_vals)
 
+%% Extract data
 % get auxiliary variables
 y = zeros(2, length(t));
 y(:,1) = x_prev;
@@ -9,26 +10,28 @@ h = t(2)-t(1);
 m1 = aux_vals.m1;
 m2 = aux_vals.m2;
 g = aux_vals.g;
-l1 = aux_vals.l1;
-l2 = aux_vals.l2;
 tol = aux_vals.tol;
-
-% calc not changing matrices
-M = diag([aux_vals.m1, aux_vals.m2]);
-M_div_h2 = M / h^2;
-
+dK1 = aux_vals.dK_dk1; % matrix
+dK2 = aux_vals.dK_dk2; % matrix
+drhs = aux_vals.df_rhs_dk; % matrix
 
 % define functions to update variables
-f_k = aux_vals.interpolate_k;
-f_rhs = @(k)([-m1*g + k(1)*l1 - k(2)*l2; ...
-         -m2*g + k(2)*l2]);
-f_K = @(k)([k(1)+k(2), -k(2); -k(2), k(2)]);
+f_k = aux_vals.interpolate_k; % function
+f_K = aux_vals.f_K; % function
+f_rhs = aux_vals.f_rhs; % function
+dk_d_dx = aux_vals.dk_d_dx; % function
+ddx_dx = aux_vals.ddx_dx; % function
+get_dx = aux_vals.get_dx; % function
 
-% fuction for newton loop
+% calc not changing matrices
+M = diag([m1, m2]);
+M_div_h2 = M / h^2;
+
+%% fuction for newton loop
 f_newton = @(y,i,K,rhs)((M_div_h2 + K) * y(:, i+1)- 2*M_div_h2*y(:,i)+ M_div_h2*y(:,i-1)-rhs);
 
 % init vars
-k = f_k(y(:,2));
+k = f_k(get_dx(y(:,2)));
 K = f_K(k);
 rhs = f_rhs(k);
 
@@ -38,32 +41,23 @@ for i = 2 : length(t)-1
     y(:,i+1) = (M_div_h2 + K) \ ((2 * M_div_h2)*y(:,i) - M_div_h2*y(:,i-1)+rhs);
     
     %update values
-    k = f_k(y(:,i+1));
+    k = f_k(get_dx(y(:,i+1)));
     K = f_K(k);
     rhs = f_rhs(k);
     
     % newton loop
     while 1
-        %dK/dk1
-        dK1 = [1, 0; 0, 0];
-        %dK/dk2
-        dK2 = [1, -1; -1, 1];
-        %dk1/dx
-        dk1 = [2*(3*y(1,i+1)- y(2,i+1)- 2*l1+ l2), 2*(y(2,i+1)-y(1,i+1)-l2)];
-        %dk2/dx
-        dk2 = [2*(6*y(1,i+1)- 5*y(2,i+1)- l1 + 5*l2), 10*(y(2,i+1)-y(1,i+1)-l2)];
+        dx = get_dx(y(:,i+1));
         %dk/dx
-        dk = [dk1; dk2];
-        %drhs/dk
-        drhs = [l1, -l2; 0, l2];
+        dk = dk_d_dx(dx);
         % J = M/h^2 + K + dK/dk1 * (dk1/dx * x) + dK/dk2 * (dk2/dx * x) - drhs/dk * dk/dx
-        J = M_div_h2 + K + dK1*(dk1*y(:,i+1))+dK2*(dk2*y(:,i+1))-drhs*dk;
+        J = M_div_h2 + K + dK1*(dk(1,:)*ddx_dx*y(:,i+1))+dK2*(dk(2,:)*ddx_dx*y(:,i+1))-drhs*dk;
         
         %newton step
         y(:,i+1) = J\(-f_newton(y,i,K,rhs)+J*y(:,i+1));
         
         %update values to check error
-        k = f_k(y(:,i+1));
+        k = f_k(dx);
         K = f_K(k);
         rhs = f_rhs(k);
         
--- matlab/2DOF_MassSpringModel/twoMassSpringModel.m
+++ matlab/2DOF_MassSpringModel/twoMassSpringModel.m
@@ -1,7 +1,7 @@
 clear; clc; close all;
 
 %% parameters
-m1 = 1; m2 = 2;
+m1 = 1; m2 = 1*2;
 k1_init = 350; k2_init = 100;
 g = 9.81;
 l1 = 1;
@@ -11,8 +11,28 @@ tol = 1e-8;
 
 
 %% generate model (k1,k2)=f(x1,x2)
-interpolate_k = @(x)([2, 1; 1, 5]*[(l1-x(1))^2; (l2-x(2)+x(1))^2]+[k1_init; k2_init]);
-                  
+quadratic_part = [2, 1; 1, 5];
+ lin_part = [-2 -3; -1, -5];
+%lin_part = [0, 0; 0, 0];
+
+interpolate_k = @(dx)(quadratic_part*[dx(1)^2; dx(2)^2]+lin_part*[dx(1);dx(2)]+[k1_init; k2_init]);
+% interpolate_k = @(dx)([2, 1; 1, 5]*[(l1-x(1))^2; (l2-x(2)+x(1))^2]+[k1_init; k2_init]);
+dk_d_dx = @(dx)(2*quadratic_part*diag(dx) + lin_part);
+
+f_rhs = @(k)([-m1*g + k(1)*l1 - k(2)*l2; ...
+              -m2*g + k(2)*l2]);
+df_rhs_dk = [l1, -l2; 0, l2];  
+        
+f_K = @(k)([k(1)+k(2), -k(2); 
+           -k(2), k(2)]);
+dK_dk1 = [1, 0; 0, 0];
+dK_dk2 = [1, -1; -1, 1];
+       
+get_dx = @(x)([l1-x(1); 
+               l2+x(1)-x(2)]);
+ddx_dx = [-1, 0; ...
+           1, -1];           
+           
 %% declare aux_vals
 aux_vals = struct('m1', m1, ...
                   'm2', m2, ...
@@ -20,6 +40,14 @@ aux_vals = struct('m1', m1, ...
                   'l2', l2, ...
                   'g',  g, ...
                   'interpolate_k', interpolate_k, ...
+                  'f_K', f_K, ...
+                  'f_rhs', f_rhs, ...
+                  'dK_dk1', dK_dk1, ...
+                  'dK_dk2', dK_dk2, ...
+                  'dk_d_dx', dk_d_dx, ...
+                  'ddx_dx', ddx_dx, ...
+                  'get_dx', get_dx, ...
+                  'df_rhs_dk', df_rhs_dk, ...
                   'tol', tol);
 
 x_prev = [l1; l1+l2]; 
@@ -32,15 +60,16 @@ t = 0:delta_t:num_iter*delta_t;
 
 %% equilibrium
 
-[y2,k2] = newton_equi(x_curr, aux_vals);
+[y2,k2] = Copy_of_newton_equi(x_curr, aux_vals);
 d1 = (l1-y2(1));
 d2 = (l2-y2(2)+y2(1));
+disp('Equilibrium solution');
 abs(d1*k2(1) - (m1+m2)*g) < tol
 abs(d2*k2(2) - m2*g) < tol
 
 %% simulation
 
-[t,y] = BW_2DOF_scheme(t, x_prev, x_curr, aux_vals);
+[t,y] = Copy_of_BW_2DOF_scheme(t, x_prev, x_curr, aux_vals);
 
 %% plots
 figure;
