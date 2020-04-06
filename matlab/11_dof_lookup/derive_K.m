002c7a2402ff28c9cfeaf49145730d2e357c47f6
ga78mov <felix-sievers@freenet.de>
2020-04-06 11:44:28 +0200

    matlab 11 dof ref files

--- matlab/11_dof_lookup/derive_K.m
+++ matlab/11_dof_lookup/derive_K.m
@@ -1,4 +1,51 @@
 clear; clc; close all;
+%% make lookup
+a = [19.32e3; 260e3; 19.32e3; 260e3; 13.12e3; 260e3; 13.12e3; 260e3] ;
+b = 1.59;
+c = 1.2;
+
+%%
+global k_grid size_grid l_min l_max dl;
+% calc min and max length to evaluate (just for now)
+l_min = 0.05;
+l_max = 0.8;
+% grid size
+size_grid = 1000;
+% grid value allocation
+global X;
+X = zeros(size_grid, 1);
+k_grid = zeros(8*size_grid,1);
+dl = (l_max-l_min)/(size_grid-1);
+% k response function
+k = @(l,a)(c*l*l + b*l + a);
+% fill in grid values
+for i = 0:size_grid-1
+    X(i+1) = l_min+i*dl;
+end
+for i = 1:8
+    for j = 1:size_grid
+        k_grid((i-1)*size_grid + j)= k(X(j), a(i));
+    end
+end
+global k_spline1 k_spline2 k_spline3 k_spline4 k_spline5 k_spline6 k_spline7 k_spline8;
+global k_der1 k_der2 k_der3 k_der4 k_der5 k_der6 k_der7 k_der8; 
+k_spline1 = spline(X,k_grid(1:size_grid));
+k_der1 = fnder(k_spline1,1);
+k_spline2 = spline(X,k_grid(size_grid+1:2*size_grid));
+k_der2 = fnder(k_spline2,1);
+k_spline3 = spline(X,k_grid(2*size_grid+1:3*size_grid));
+k_der3 = fnder(k_spline3,1);
+k_spline4 = spline(X,k_grid(3*size_grid+1:4*size_grid));
+k_der4 = fnder(k_spline4,1);
+k_spline5 = spline(X,k_grid(4*size_grid+1:5*size_grid));
+k_der5 = fnder(k_spline5,1);
+k_spline6 = spline(X,k_grid(5*size_grid+1:6*size_grid));
+k_der6 = fnder(k_spline6,1);
+k_spline7 = spline(X,k_grid(6*size_grid+1:7*size_grid));
+k_der7 = fnder(k_spline7,1);
+k_spline8 = spline(X,k_grid(7*size_grid+1:8*size_grid));
+k_der8 = fnder(k_spline8,1);
+
 %%
 global l_long_fl l_long_fr l_long_rl l_long_rr l_lat_fl l_lat_fr l_lat_rl l_lat_rr;
 %% symbolic derivative
@@ -34,20 +81,13 @@ dK10_dk = [diff(K(:,10), k(1)), diff(K(:,10), k(2)), diff(K(:,10), k(3)), diff(K
 dK11_dk = [diff(K(:,11), k(1)), diff(K(:,11), k(2)), diff(K(:,11), k(3)), diff(K(:,11), k(4)), diff(K(:,11), k(5)), diff(K(:,11), k(6)), diff(K(:,11), k(7)), diff(K(:,11), k(8))];
 dKcols_dk = [dK1_dk, dK2_dk, dK3_dk, dK4_dk, dK5_dk, dK6_dk, dK7_dk, dK8_dk, dK9_dk, dK10_dk, dK11_dk];
 
-lookup_struct = load('lookup_table.mat');
-lookup_table = lookup_struct.big_lookup_table;
-global k_grid size_grid l_min l_max dl;
-size_grid = length(lookup_table);
-l_min = lookup_table(1,1);
-l_max = lookup_table(size_grid,1);
-dl = (l_max - l_min)/(size_grid-1);
-k_grid = lookup_table(:,2:9);
+
 
 %% parameters
 % time
 num_iter = 1000;
-delta_t = 1e-5; 
-t = 0:delta_t:num_iter*delta_t;
+delta_t = 1e-3; 
+t = 0:delta_t:(num_iter-1)*delta_t;
 
 
 tol = 1e-12;
@@ -59,32 +99,32 @@ l_long_fl=1.395;
 l_long_fr=1.395;
 l_long_rl=1.596;
 l_long_rr=1.596;
-l_lat_fl=2*0.8458;
-l_lat_fr=2*0.8458;
-l_lat_rl=2*0.84;
-l_lat_rr=2*0.84;
+l_lat_fl=1.6916;
+l_lat_fr=1.6916;
+l_lat_rl=1.68;
+l_lat_rr=1.68;
 mass_Body=1936;
 I_body_xx=640;
 I_body_yy=4800;
-mass_wheel_fl=145/2;
-mass_tyre_fl=0;
-mass_wheel_fr=145/2;
-mass_tyre_fr=0;
-mass_wheel_rl=135/2;
-mass_tyre_rl=0;
-mass_wheel_rr=135/2;
-mass_tyre_rr=0;
-g = 1;
+mass_wheel_fl=67.5;
+mass_tyre_fl=30;
+mass_wheel_fr=67.5;
+mass_tyre_fr=30;
+mass_wheel_rl=67.5;
+mass_tyre_rl=30;
+mass_wheel_rr=67.5;
+mass_tyre_rr=30;
+g = 0;
 u_n = [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0];
-u_n = rand(11,1);
 
 y(:,1) = u_n;
 u_n_m_1=u_n;
 u_n_p_1=u_n;
 
-rhs = -[mass_Body; 0; 0; mass_wheel_fl; mass_tyre_fl; mass_wheel_fr; mass_tyre_fr; mass_wheel_rl; mass_tyre_rl; mass_wheel_rr; mass_tyre_rr]  * g;
+rhs =[1.1e3; zeros(10,1)];
 M = diag([mass_Body, I_body_xx, I_body_yy, mass_wheel_fl, mass_tyre_fl, mass_wheel_fr, mass_tyre_fr, mass_wheel_rl, mass_tyre_rl, mass_wheel_rr, mass_tyre_rr]);
 M_div_h2 = M / (delta_t * delta_t);
+
 %% time steps
 
 f_newton = @(y_curr,y1,y2,K)( ( M_div_h2 + K ) * y_curr - 2 * M_div_h2 * y1 + M_div_h2 * y2 - rhs);
@@ -148,6 +188,7 @@ function K = get_K(x)
     global l_lat_fr;
     global l_lat_rl;
     global l_lat_rr;
+    car = x(1);
     upper_fl = x(4);
     lower_fl = x(5);
     upper_fr = x(6);
@@ -156,15 +197,26 @@ function K = get_K(x)
     lower_rl = x(9);
     upper_rr = x(10);
     lower_rr = x(11);
+    global k_spline1 k_spline2 k_spline3 k_spline4 k_spline5 k_spline6 k_spline7 k_spline8;
     
-    k_body_fl=interpolate(upper_fl - lower_fl, 1);
-    k_tyre_fl=interpolate(lower_fl, 2);
-    k_body_fr=interpolate(upper_fr-lower_fr, 3);
-    k_tyre_fr=interpolate(lower_fr, 4);
-    k_body_rl=interpolate(upper_rl-lower_rl, 5);
-    k_tyre_rl=interpolate(lower_rl, 6);
-    k_body_rr=interpolate(upper_rr-lower_rr, 7);
-    k_tyre_rr=interpolate(lower_rr, 8);
+    %global X k_grid size_grid;
+    %k_body_fl=interp1(X,k_grid(1:size_grid),0.5 - upper_fl + car);
+    %k_tyre_fl=interp1(X,k_grid(size_grid+1:2*size_grid),0.5 + upper_fl - lower_fl);
+    %k_body_fr=interp1(X,k_grid(2*size_grid+1:3*size_grid),0.5 - upper_fr + car);
+    %k_tyre_fr=interp1(X,k_grid(3*size_grid+1:4*size_grid),0.5 + upper_fr - lower_fr);
+    %k_body_rl=interp1(X,k_grid(4*size_grid+1:5*size_grid),0.5 - upper_rl + car);
+    %k_tyre_rl=interp1(X,k_grid(5*size_grid+1:6*size_grid),0.5 + upper_rl -lower_rl);
+    %k_body_rr=interp1(X,k_grid(6*size_grid+1:7*size_grid),0.5 - upper_rr + car);
+    %k_tyre_rr=interp1(X,k_grid(7*size_grid+1:8*size_grid),0.5 + upper_rr - lower_rr);
+    
+    k_body_fl=fnval(k_spline1,0.5 - upper_fl + car);
+    k_tyre_fl=fnval(k_spline2,0.5 + upper_fl - lower_fl);
+    k_body_fr=fnval(k_spline3,0.5 - upper_fr + car);
+    k_tyre_fr=fnval(k_spline4,0.5 + upper_fr - lower_fr);
+    k_body_rl=fnval(k_spline5,0.5 - upper_rl + car);
+    k_tyre_rl=fnval(k_spline6,0.5 + upper_rl -lower_rl);
+    k_body_rr=fnval(k_spline7,0.5 - upper_rr + car);
+    k_tyre_rr=fnval(k_spline8,0.5 + upper_rr - lower_rr);
     
     K = [k_body_fl+k_body_fr+k_body_rl+k_body_rr, k_body_fl*l_lat_fl-k_body_fr*l_lat_fr+k_body_rl*l_lat_rl-k_body_rr*l_lat_rr, -k_body_fl*l_long_fl-k_body_fr*l_long_fr+k_body_rl*l_long_rl+k_body_rr*l_long_rr,  -k_body_fl, 0, -k_body_fr, 0, -k_body_rl, 0, -k_body_rr, 0;
    0, l_lat_fl*l_lat_fl*k_body_fl+l_lat_fr*l_lat_fr*k_body_fr+l_lat_rl*l_lat_rl*k_body_rl+l_lat_rr*l_lat_rr*k_body_rr, -l_long_fl*l_lat_fl*k_body_fl+l_lat_fr*l_long_fr*k_body_fr+l_long_rl*l_lat_rl*k_body_rl-l_long_rr*l_lat_rr*k_body_rr, -l_lat_fl*k_body_fl, 0, l_lat_fr*k_body_fr, 0, -l_lat_rl*k_body_rl, 0, l_lat_rr*k_body_rr, 0;
@@ -183,69 +235,50 @@ end
 function dk = getdk_dx(x) % 8x11
     dk = zeros(8,11);
     
-    dk1_dl = deriv_dk(x(4)-x(5), 1);
-    dk2_dl = deriv_dk(x(5), 2);
-    dk3_dl = deriv_dk(x(6)-x(7), 3);
-    dk4_dl = deriv_dk(x(7), 4);
-    dk5_dl = deriv_dk(x(8)-x(9), 5);
-    dk6_dl = deriv_dk(x(9), 6);
-    dk7_dl = deriv_dk(x(10)-x(11), 7);
-    dk8_dl = deriv_dk(x(11), 8);
+    car = x(1);
+    upper_fl = x(4);
+    lower_fl = x(5);
+    upper_fr = x(6);
+    lower_fr = x(7);
+    upper_rl = x(8);
+    lower_rl = x(9);
+    upper_rr = x(10);
+    lower_rr = x(11);
     
-    dk(1,4) = dk1_dl;
-    dk(1,5) = -dk1_dl;
-    dk(2,5) = dk2_dl;
-    dk(3,6) = dk3_dl;
-    dk(3,7) = -dk3_dl;
-    dk(4,7) = dk4_dl;
-    dk(5,8) = dk5_dl;
-    dk(5,9) = -dk5_dl;
-    dk(6,9) = dk6_dl;
-    dk(7,10) = dk7_dl;
-    dk(7,11) = -dk7_dl;
-    dk(8,11) = dk8_dl;
-end
-
-% interpolates k_grid to a certain value l
-function k = interpolate(l, s)
-    global k_grid l_min l_max dl;
-    if (l < l_min)
-        l = l_min;
-    end
-    if (l > l_max) 
-       l = l_max; 
-    end
-    % matlab need a +1 
-    % we dont need it in c++ tho
-    idx = (l-l_min)/dl + 1;
-    if (floor(idx) == idx)
-        k = k_grid(idx,s);
-    else
-        i_low = floor(idx);
-        low = k_grid(i_low, s);
-        top = k_grid(ceil(idx), s);
-        pr = (l-(l_min+dl*(i_low-1)))/dl; % percentage from left to right
-        k = (1-pr)*low+pr*top;
-    end
-end
-
-% calculates the derivative at a certain position of the grid
-function dk = deriv_dk(l, s)
-    global k_grid l_min l_max dl;
-    if l <= l_min
-       l = l_min + 1e-12; 
-    end
-    if l >= l_max
-       l = l_max - 1e-12; 
-    end
-    idx = (l-l_min)/dl + 1;
-    if floor(idx) == idx
-        low = k_grid(idx-1, s);
-        top = k_grid(idx+1, s);
-        dk = (top-low)/(2*dl);
-    else
-        low = k_grid(floor(idx), s);
-        top = k_grid(ceil(idx), s);
-        dk = (top-low)/dl;
-    end
+    %dk1_dl = deriv_dk(x(4)-x(5), 1);
+    %dk2_dl = deriv_dk(x(5), 2);
+    %dk3_dl = deriv_dk(x(6)-x(7), 3);
+    %dk4_dl = deriv_dk(x(7), 4);
+    %dk5_dl = deriv_dk(x(8)-x(9), 5);
+    %dk6_dl = deriv_dk(x(9), 6);
+    %dk7_dl = deriv_dk(x(10)-x(11), 7);
+    %dk8_dl = deriv_dk(x(11), 8);
+    
+    global k_der1 k_der2 k_der3 k_der4 k_der5 k_der6 k_der7 k_der8; 
+    
+    dk1_dl = fnval(k_der1, 0.5 - upper_fl + car);
+    dk2_dl = fnval(k_der2, 0.5 + upper_fl - lower_fl);
+    dk3_dl = fnval(k_der3, 0.5 - upper_fr + car);
+    dk4_dl = fnval(k_der4, 0.5 + upper_fr - lower_fr);
+    dk5_dl = fnval(k_der5, 0.5 - upper_rl + car);
+    dk6_dl = fnval(k_der6, 0.5 + upper_rl -lower_rl);
+    dk7_dl = fnval(k_der7, 0.5 - upper_rr + car);
+    dk8_dl = fnval(k_der8, 0.5 + upper_rr - lower_rr);
+    
+    dk(1,1) = dk1_dl;
+    dk(1,4) = -dk1_dl;
+    dk(2,4) = dk2_dl;
+    dk(2,5) = -dk2_dl;
+    dk(3,1) = dk3_dl;
+    dk(3,6) = -dk3_dl;
+    dk(4,6) = dk4_dl;
+    dk(4,7) = -dk4_dl;
+    dk(5,1) = dk5_dl;
+    dk(5,8) = -dk5_dl;
+    dk(6,8) = dk6_dl;
+    dk(6,9) = -dk6_dl;
+    dk(7,1) = dk7_dl;
+    dk(7,10) = -dk7_dl;
+    dk(8,10) = dk8_dl;
+    dk(8,11) = -dk8_dl;
 end
