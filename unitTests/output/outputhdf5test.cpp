a0c745be382dd72282c85ac657106afa6317e80c
Teodor Rotaru <teodor.rotaru@tum.de>
2020-05-02 12:57:57 +0200

    Start outputHDF5

---
+++ unitTests/output/outputhdf5test.cpp
@@ -0,0 +1,20 @@
+#include "outputhdf5.h"
+#include "gtest/gtest.h"
+
+#include <array>
+
+class OutputTest : public ::testing::Test {};
+
+
+TEST_F(OutputTest, writeHDF5file) {
+	const int vec_dim = 6;
+	std::array<double, vec_dim> vec{ 5, 9, 91, 26, 5, 94 };
+	std::array<double, vec_dim> vec_another;
+
+	writeVectorToFile("output1.h5", "vec2write", vec.data(), vec_dim);
+	readVectorFromFile("output1.h5", "vec2write", vec_another.data(), vec_dim);
+
+	for (int i = 0; i < vec.size(); ++i) {
+		EXPECT_EQ(vec[i], vec_another[i]) << "Vectors differ at index " << i;
+	}
+}
