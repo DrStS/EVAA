97fa819346f667ce1aa59b6da1eab1807c066836
Teodor Rotaru <teodor.rotaru@tum.de>
2020-05-12 16:54:59 +0200

    Edit BenchMBDALE

--- benchmarkTests/compareMethods/BenchMBDALE.cpp
+++ benchmarkTests/compareMethods/BenchMBDALE.cpp
@@ -1,33 +1,46 @@
 #include <benchmark/benchmark.h>
 #include <mkl.h>
 #include <iostream>
-#include "EAAAComputeEngine.h"
+
+#include <EVAAComputeEngine.h>
 
 using namespace EVAA;
 
-class Setup
-{
-    Setup()
-    {
-        // call your setup function
-        std::cout << "singleton ctor called only once in the whole program" << std::endl;
-        /// create db
-        db = MetaDatabase<Constants::floatEVAA>::getDatabase();
+// car
+std::string carWithInterpolationFileNameXML =
+    "C:\\software\\repos\\EVAA\\inputFiles\\CarWithInterpolation.xml";
+std::string carConstantStiffnessFileNameXML =
+    "C:\\software\\repos\\EVAA\\inputFiles\\CarConstantStiffness.xml";
+
+/** Hardcoded paths for load profiles*/
+std::string loadArbitraryFileNameXML =
+    "C:\\software\\repos\\EVAA\\inputFiles\\LoadArbitraryCar.xml";
+std::string loadCircularFileNameXML =
+    "C:\\software\\repos\\EVAA\\inputFiles\\LoadCircularCar.xml";
+std::string loadStraightFileNameXML =
+    "C:\\software\\repos\\EVAA\\inputFiles\\LoadStraightCar.xml";
+
+/** Hardcoded path for simulation parameters*/
+std::string simulationParametersFileNameXML =
+    "C:\\software\\repos\\EVAA\\inputFiles\\SimulationParameters.xml";
+
+static void BM_MBD(benchmark::State& state) {
+    std::string loadProfileFileNameXML = loadCircularFileNameXML;
+    std::string simulationParametersFileNameXML = "SimulationParameters.xml";
+#ifdef INTERPOLATION
+    std::string carSettingsFileNameXML = carWithInterpolationFileNameXML;
+#else
+    std::string carSettingsFileNameXML = carConstantStiffnessFileNameXML;
+#endif
+    EVAA::EVAAComputeEngine* myComputeEngine = new EVAA::EVAAComputeEngine(
+        simulationParametersFileNameXML, carSettingsFileNameXML, loadProfileFileNameXML);
+
+    auto& db = MetaDatabase<Constants::floatEVAA>::getDatabase();
+    db.readSimulationParameters(simulationParametersFileNameXML);
+    
+    for (auto _ : state) {
+        // ...-
     }
-
-public:
-    MetaDatabase<Constants::floatEVAA>& db;
-
-    static void PerformSetup(const std::string& loadFilename)
-    {
-        static Setup setup;
-        db.readLoadParameters(loadFilename);
-    }
-};
-
-static void BM_SomeFunction(benchmark::State& state) {
-    Setup::PerformSetup()
-        for (auto _ : state) {
-            // ...
-        }
+    BENCHMARK(BM_MBD)->UseRealTime()->Arg(10)->Arg(15)->Arg(20);
+    delete myComputeEngine;
 }
