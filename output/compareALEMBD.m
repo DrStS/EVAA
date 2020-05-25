d9cbc3f6101e9ee04ba929c64d2fef30c4fa15d1
Raffi <raffael.duell@gmx.de>
2020-05-25 14:29:00 +0300

    Make DFT analysis of the results

--- output/compareALEMBD.m
+++ output/compareALEMBD.m
@@ -56,6 +56,24 @@ normed_pos = vecnorm(diff_all(2:end,index:end),2,2);
 
 t = delta_t * (1:size(diff_all(2:end,:), 1)); 
 
+%compute the DFT
+mbdDFT = fft(mbdSolution(2:end,36) - mean(mbdSolution(2:end,36)));
+aleDFT = fft(aleSolution(2:end,36) - mean(aleSolution(2:end,36)));
+sampleFrequency = 1 / delta_t;
+L = length(aleSolution(2:end,36));
+
+P2 = abs(mbdDFT/L);
+mbdDFT = P2(1:L/2+1);
+mbdDFT(2:end-1) = 2*mbdDFT(2:end-1);
+
+
+P2 = abs(aleDFT/L);
+aleDFT = P2(1:L/2+1);
+aleDFT(2:end-1) = 2*aleDFT(2:end-1);
+
+frequency = sampleFrequency * (0:(L/2))/L;
+
+
 close all
 figure
 plot(t,diff_all(2:end,index + 0))
@@ -87,9 +105,10 @@ xlabel('time [s]');
 figure
 plot(t,diff_all(2:end,index + 0))
 hold on
+plot(t,diff_all(2:end,index + 2))
 plot(t,mbdSolution(2:end,36)-mbdSolution(1,36))
-legend('X','Z')
-title('Deviation from the trajectory for MBD')
+legend('X','Y','Z')
+title('Deviation from the trajectory for MBD, Ixx=6400, Iyy=Izz=6400')
 ylabel('difference [m]')
 xlabel('time [s]')
 axis([0 50 -0.08 0.01])
@@ -97,12 +116,22 @@ axis([0 50 -0.08 0.01])
 figure
 plot(t,zeros(length(t),1))  % zero due to the Lagrangiabn formulation
 hold on
+plot(t,zeros(length(t),1))  % zero due to the Lagrangiabn formulation
 plot(t,aleSolution(2:end,36)-aleSolution(2,36))
-legend('X','Z')
-title('Deviation from the trajectory for ALE')
+legend('X','Y','Z')
+title('Deviation from the trajectory for ALE, Ixx=6400, Iyy=Izz=4800')
 ylabel('difference [m]')
 xlabel('time [s]')
 axis([0 50 -0.08 0.01])
+
+figure
+plot(frequency(1:end), aleDFT(1:end))
+hold on
+plot(frequency(1:end), mbdDFT(1:end))
+legend('MBD','ALE')
+title('Amplitude spectrum of the Z-component of the center of gravity')
+xlabel('f [Hz]')
+ylabel('|P1(f)|')
 
 
 
