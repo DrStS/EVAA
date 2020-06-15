6bcae47e45240a64226b81cda8ff1e05975d0328
NicolaZi <nicola.zimmermann@tum.de>
2020-06-15 14:15:44 +0200

    header added

--- testRefs/matlab/linear11DOFBackwardEuler/BEoneFile01.m
+++ testRefs/matlab/linear11DOFBackwardEuler/BEoneFile01.m
@@ -1,6 +1,41 @@
 %%
+% Copyright &copy; 2020, Nicola Zimmermann, Munich \n
+% All rights reserved. \n
+% 
+% This file is part of EVAA.
+% 
+% EVAA is free software: you can redistribute it and/or modify \n
+% it under the terms of the GNU General Public  License as published by \n
+% the Free Software Foundation, either version 3 of the License, or \n
+% (at your option) any later version. \n
+% 
+% EVAA is distributed in the hope that it will be useful, \n
+% but WITHOUT ANY WARRANTY; without even the implied warranty of \n
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the \n
+% GNU General Public License for more details. \n
+% 
+% You should have received a copy of the GNU General Public License \n
+% along with EVAA.  If not, see <a
+% href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
+% 
+% Additional permission under GNU GPL version 3 section 7
+% 
+% If you modify this Program, or any covered work, by linking or combining it
+% with Intel Math Kernel Libraries(MKL) (or a modified version of that
+% library), containing parts covered by the terms of the license of the MKL,
+% the licensors of this Program grant you additional permission to convey the
+% resulting work.
+% 
+% DESCRIPTION
+% This file is used to compute displacements/ rotations
+% of the 11-Dof 2-track-model
+% with a backward Euler time integration scheme 
+% for linear spring stiffness
+
+
+%%
 clc;
-clear all;
+clear;
 close all;
 format long e;
 % Dynamic Backward Euler problem 
