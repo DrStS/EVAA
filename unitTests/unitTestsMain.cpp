7ea82690370e91e0739dadb6b04f54aaa818c4a1
Stefan Sicklinger <mail@sicklinger.com>
2021-01-25 21:23:43 +0100

    added gmock

--- unitTests/unitTestsMain.cpp
+++ unitTests/unitTestsMain.cpp
@@ -35,10 +35,10 @@
  *
  *
  **************************************************************************************************/
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+        ::testing::InitGoogleMock(&argc, argv);
+  return RUN_ALL_TESTS();
 }
 
