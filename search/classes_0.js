64e2f07c35b00b033e359e0ca6b8932d3b24a86c
DrStS <DrStS@users.noreply.github.com>
2021-03-03 22:09:29 +0000

    deploy: 40425f5ddea8769415593f00ec03d1a77e5ab767

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['auxiliaryfunctions_30',['AuxiliaryFunctions',['../class_e_v_a_a_1_1_auxiliary_functions.html',1,'EVAA']]],
-  ['auxiliaryparameters_31',['AuxiliaryParameters',['../class_e_v_a_a_1_1_auxiliary_parameters.html',1,'EVAA']]]
+  ['auxiliaryfunctions_234',['AuxiliaryFunctions',['../class_e_v_a_a_1_1_auxiliary_functions.html',1,'EVAA']]],
+  ['auxiliaryparameters_235',['AuxiliaryParameters',['../class_e_v_a_a_1_1_auxiliary_parameters.html',1,'EVAA']]]
 ];
---
+++ search/typedefs_11.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['vehicle_5ftraits_452',['Vehicle_traits',['../class_e_v_a_a__t.html#a0737f5b76869c75f79b54c1c3f027c69',1,'EVAA_t']]],
+  ['vehicle_5ftype_453',['Vehicle_type',['../class_e_v_a_a__t.html#a4d586fa3f7f145761d7d88bdf6212340',1,'EVAA_t']]]
+];
